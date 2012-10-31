ebecc34ae9d75c98e7fa2d34f34534b9572e5f06
Lane Aasen <laneaasen@gmail.com>
2012-10-30 21:51:04 -0700

    adds start_server which works

--- mcnode.py
+++ mcnode.py
@@ -7,12 +7,16 @@
 
 import urllib
 import subprocess
+import os
 
 # configuration that may later be put into a file
 # dictionaries are awesome for this!!!
 config = { 
 	'server_jar_url' : 'https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar',
-	'server_jar_path' : './minecraft_server.jar'
+	'server_directory' : './server/',
+	'server_jar_path' : 'minecraft_server.jar',
+	'init_memory' : '512M',
+	'max_memory' : '1024M'
 }
 
 # logs an arbitrary message to something
@@ -28,8 +32,11 @@ def __get_remote_file(src, dest):
 		__log('__get_remote_file failed to retrieve ' + src)
 
 # updates minecraft_sserver.jar with a fresh version from Minecraft's CDN
-def update_server():
-	__get_remote_file(config['server_jar_url'], config['server_jar_path'])
+def update_server(src=config['server_jar_url'], dest_dir=config['server_directory'], dest=config['server_jar_path']):
+	if not os.path.exists(dest_dir):
+		os.makedirs(dest_dir)
+
+	__get_remote_file(src, dest_dir + dest)
 
 # returns true if the os supports java from the command line
 # TODO: make quiet
@@ -42,9 +49,14 @@ def __check_path():
 	return False
 
 # spins up the server
-def start_server():
-	if not __check_path():
-		return
+def start_server(server_directory=config['server_directory'], init_memory=config['init_memory'], max_memory=config['max_memory'], server_jar_path=config['server_jar_path']):
+	# if not __check_path():
+	# 	return
 
-# update_server()
+	os.chdir(server_directory)
+	subprocess.call(['java', '-Xms' + init_memory, '-Xmx' + max_memory, '-jar', server_jar_path, 'nogui'])
+
+
+
+update_server()
 start_server()
