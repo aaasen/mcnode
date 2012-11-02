2999e940b9499e03a22e8707d9936979a69f0341
Lane Aasen <laneaasen@gmail.com>
2012-11-02 11:28:32 -0700

    adds init() and post() to MCNotifier. going to make a test client app now for testing

--- mcnotifier.py
+++ mcnotifier.py
@@ -6,3 +6,13 @@
 # mcnotifier
 # notifies mcnode-compatible servers of updates to the mcnode
 # this can be used to easily create web apps for minecraft servers
+
+import requests
+
+class MCNotifier:
+
+	def post(self, data=None):
+		return requests.post(self.url, data)
+
+	def __init__(self, url):
+		self.url = url
