397c6b55bcf94013fbc41a9a3f6b45ec6f74a295
Lane Aasen <laneaasen@gmail.com>
2012-11-02 11:19:58 -0700

    adds mcnotifier header comment

---
+++ mcnotifier.py
@@ -0,0 +1,8 @@
+# @aaaasen (Lane Aasen)
+# November 2012
+# Python 2.7
+# Tested on (Arch) Linux
+
+# mcnotifier
+# notifies mcnode-compatible servers of updates to the mcnode
+# this can be used to easily create web apps for minecraft servers
