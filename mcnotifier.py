1dbb06b573e6517f548f045d801f48c9555074e3
Lane Aasen <laneaasen@gmail.com>
2012-11-02 22:04:21 -0700

    adds url sanitization and disconnect update for status bot

--- mcnotifier.py
+++ mcnotifier.py
@@ -9,10 +9,15 @@
 
 import requests
 
+requests.max_redirects = 1024
+
 class MCNotifier:
 
-	def post(self, data=None):
-		return requests.post(self.url, data)
+	def post(self, data=None, url=None):
+		return requests.post(url if url else self.url, data)
+
+	def put(self, data=None, url=None):
+		return requests.put(url if url else self.url, data)
 
 	def __init__(self, url):
-		self.url = url
+		self.url = url + ('' if url[-1] == '/' else '/')
--- status_bot.py
+++ status_bot.py
@@ -12,8 +12,15 @@ from mcnotifier import MCNotifier
 class StatusBot(MCBot):
 	"""a bot that says hello to people when they log in"""
 
+	def get_url(self, username):
+		return self.mcnotifier.url + username
+
 	def on_connect(self, data):
 		self.mcnotifier.post({ 'player[username]' : data[3], 'player[online]' : 'true' })
+		self.mcnotifier.put({ 'player[username]' : data[3], 'player[online]' : 'true' }, self.get_url(data[3]))
+
+	def on_disconnect(self, data):
+		self.mcnotifier.put({ 'player[username]' : data[3], 'player[online]' : 'false' }, self.get_url(data[3]))
 
 	def __init__(self, node, url):
 		self.node = node
